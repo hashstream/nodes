6998d7a2736451c7de35381b313e562281a93006
Kieran <kieran@harkin.me>
2018-03-28 00:03:45 +0800

    fix formatting + get user location

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import 'ol/ol.css';
+import ol from 'ol';
 import proj from 'ol/proj';
 import extent from 'ol/extent';
 import Map from 'ol/map';
@@ -12,45 +13,56 @@ import loadingstrategy from 'ol/loadingstrategy';
 import OSM from 'ol/source/osm';
 
 var vs = new Vector({
-			format: new GeoJSON(),
-			loader: function(ext, resolution, projection) {
-					var pt = proj.transform(extent.getCenter(ext), projection.getCode(), 'EPSG:4326');
-					var xhr = new XMLHttpRequest();
-					 xhr.open('GET', '/api.php?long=' + pt[0] + '&lat=' + pt[1] + '&range=' + (extent.getWidth(ext) / proj.METERS_PER_UNIT.m / 1000) / 2);
-					 var onError = function() {
-					   vs.removeLoadedExtent(ext);
-					 }
-					 xhr.onerror = onError;
-					 xhr.onload = function() {
-					   if (xhr.status == 200) {
-						 vs.clear();
-						 vs.addFeatures(vs.getFormat().readFeatures(xhr.responseText, {
-							dataProjection: "EPSG:4326",
-							featureProjection: projection.getCode()
-						 }));
-					   } else {
-						 onError();
-					   }
-					 }
-					 xhr.send();
-			},
-			strategy: loadingstrategy.bbox
-		});
-		
-window.map = new Map({
-  target: 'heatmap',
-  layers: [
-  new Tile({
-	source: new OSM()
-  }),
-  new Heatmap({
-		weight: "1",
-		source: vs,
-		zIndex: 1
-	})
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 6
-  })
+    format: new GeoJSON(),
+    loader: function(ext, resolution, projection) {
+        var pt = proj.transform(extent.getCenter(ext), projection.getCode(), 'EPSG:4326');
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/api.php?long=' + pt[0] + '&lat=' + pt[1] + '&range=' + (extent.getWidth(ext) / proj.METERS_PER_UNIT.m / 1000) / 2);
+        var onError = function() {
+            vs.removeLoadedExtent(ext);
+        }
+        xhr.onerror = onError;
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                vs.clear();
+                vs.addFeatures(vs.getFormat().readFeatures(xhr.responseText, {
+                    dataProjection: "EPSG:4326",
+                    featureProjection: projection.getCode()
+                }));
+            } else {
+                onError();
+            }
+        }
+        xhr.send();
+    },
+    strategy: loadingstrategy.bbox
 });
+
+window.map = new Map({
+    target: 'heatmap',
+    layers: [
+        new Tile({
+            source: new OSM()
+        }),
+        new Heatmap({
+            weight: "1",
+            source: vs,
+            zIndex: 1
+        })
+    ],
+    view: new View({
+        center: [0, 0],
+        zoom: 2
+    })
+});
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position) {
+    var vw = map.getView();
+    var pt = proj.transform([position.coords.longitude, position.coords.latitude], 'EPSG:4326', vw.getProjection().getCode());
+    vw.setCenter(pt);
+    vw.setZoon(8);
+}
